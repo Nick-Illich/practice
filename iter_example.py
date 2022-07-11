38dddc4e197088dda8305cad0135757b541895be
Nick Illich <nickillich@yahoo.com>
2022-07-11 20:04:09 +0300

    iterator example

---
+++ iter_example.py
@@ -0,0 +1,10 @@
+my_list = ["a", 1, 10, 15, "b", (14, 88)]  # iterable
+my_list_it = iter(my_list)  # creating iterator
+print(next(my_list_it))
+print(next(my_list_it))
+print(next(my_list_it))
+print(next(my_list_it))
+print(next(my_list_it))
+print(next(my_list_it))
+print(next(my_list_it))
+
